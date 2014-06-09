6fbcb3edcaf3edec63230308f123c1e03417967d
Benjamin ter Kuile <bterkuile@gmail.com>
2014-06-09 14:37:42 +0200

    Add comment for ghost attributes

--- lib/couch_potato/persistence/ghost_attributes.rb
+++ lib/couch_potato/persistence/ghost_attributes.rb
@@ -4,6 +4,7 @@ module CouchPotato
       if(value = _document && _document[name.to_s])
         value
       else
+        # this may raise that super is not defined, but that is good since method missing should raise if nothing is found.
         super
       end
     end
